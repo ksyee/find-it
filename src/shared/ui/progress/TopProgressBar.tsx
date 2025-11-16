fc4e3150e7f6ca9fd9ed37f815323d7bc55753a7
ksy <tjssud0647@gmail.com>
2025-11-16 19:38:08 +0900

    [fix] 모바일에서 프로그레스바가 헤더에 가려지는 문제 수정

--- src/shared/ui/progress/TopProgressBar.tsx
+++ src/shared/ui/progress/TopProgressBar.tsx
@@ -4,7 +4,7 @@ const TopProgressBar = () => {
   const { isActive, progress } = useRouteProgress();
 
   return (
-    <div className="pointer-events-none fixed left-0 right-0 top-0 z-[60]">
+    <div className="pointer-events-none fixed left-0 right-0 top-0 z-[10050]">
       <div
         className={`h-1 w-full transform bg-transparent transition-opacity duration-200 ${
           isActive ? 'opacity-100' : 'opacity-0'
